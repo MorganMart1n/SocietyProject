1580ac5c74af05ccabe1601d10ef3f707dfb0dc4
Morgan Martin <63368697+MorganMart1n@users.noreply.github.com>
2024-03-28 14:08:47 +0000

    Added 3 more pages for criteria
    added final page, as well as create account and login pages with validation

--- js/Script.js
+++ js/Script.js
@@ -1,4 +1,22 @@
+
+
 function Submitted()
 {
     alert("Form Submitted, good job!")
+    
+}
+function Login()
+{
+    alert("You've logged in")
+    reDirect()
+
+}function Create()
+{
+    alert("You've created an Account, Your now logged in")
+    reDirect()
+}
+function reDirect()
+{
+    alert("Form Submitted, good job!")
+    window.location.href = "index.html";
 }
