a41719605d00f50c29892e7f35dc41fc86f267b9
Morgan Martin <63368697+MorganMart1n@users.noreply.github.com>
2024-04-14 16:18:02 +0100

    Final update
    Changes to comments, image descriptions as well as some better indentation

--- js/Script.js
+++ js/Script.js
@@ -1,22 +1,24 @@
-
-
-function Submitted()
-{
+//Alerts user with popup modal REFERENCE - https://www.w3schools.com/jsref/met_win_alert.asp
+function Submitted(){
     alert("Form Submitted, good job!")
-    
 }
-function Login()
-{
-    alert("You've logged in")
-    reDirect()
 
-}function Create()
-{
-    alert("You've created an Account, Your now logged in")
-    reDirect()
-}
-function reDirect()
-{
-    alert("Form Submitted, good job!")
+//Redirects user based on if login or creatation of account successful REFERENCE - https://www.w3schools.com/howto/howto_js_redirect_webpage.asp
+function redirect(){
     window.location.href = "index.html";
+window.location.replace("index.html");
 }
+
+//Alerts user with popup modal REFERENCE - https://www.w3schools.com/jsref/met_win_alert.asp
+function login(){
+    alert("You've logged in");
+    redirect();
+}
+
+//Alerts user with popup modal REFERENCE - https://www.w3schools.com/jsref/met_win_alert.asp
+function createAccount(){
+    alert("You've created an account. You are now logged in.");
+    redirect();
+}
+
+
